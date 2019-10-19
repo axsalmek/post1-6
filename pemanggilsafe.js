f5baaef80aba704ac5640c148593cb15b96b2f61
axsalmek <56443790+axsalmek@users.noreply.github.com>
2019-10-19 20:32:53 +0700

    Update pemanggilsafe.js

--- pemanggilsafe.js
+++ pemanggilsafe.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="/2019/10/danger-arising-from-vape.html"}
 if(x=="3"){xxx="/2019/10/this-is-way-you-can-do-to-fatten-your.html"}
 if(x=="4"){xxx="/2019/10/danger-of-vehicle-smoke-haze-for-your.html"}
 if(x=="5"){xxx="/2019/10/follow-these-methods-to-avoid-obesity.html"}
-if(x=="6"){xxx="/2019/10/good-diet-tips-for-body-health.html"}a_to_ve[i].href="https://healthkesehataninfo.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="/2019/10/good-diet-tips-for-body-health.html"}a_to_ve[i].href="https://lifehealtnew.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
