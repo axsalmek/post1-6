3184b86966dc7f2c37ceb7a07e240c59c9ee3fb0
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2020-12-26 23:43:34 +0700

    Update pemanggilsafe.js

--- pemanggilsafe.js
+++ pemanggilsafe.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="/2020/12/cara-mengamankan-link.html"}
 if(x=="2"){xxx="/2020/12/bagaimana-menulis-rencana-bisnis-untuk.html"}
 if(x=="3"){xxx="/2020/12/cara-memulai-bisnis-online-di-2020.html"}
-if(x=="4"){xxx="/2020/12/top-bussiness-ideas-for-2020.html"}a_to_ve[i].href="https://kyasafel.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/2020/12/top-bussiness-ideas-for-2020.html"}a_to_ve[i].href="https://axsalrubiansyah.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
