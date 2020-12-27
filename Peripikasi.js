be54984c8859579fc0a7c92b2c4afb7eded74ce1
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2020-12-28 00:19:01 +0700

    Update Peripikasi.js

--- Peripikasi.js
+++ Peripikasi.js
@@ -21,8 +21,8 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="2020/12/cara-menulis-presentasi-rencana-bisnis.html"}
-if(x=="2"){xxx="2020/12/cara-membangun-bisnis-bagaimana-mulai.html"}a_to_ve[i].href="https://axsalrubiansyah.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2020/12/cara-menulis-presentasi-rencana-bisnis.html"}
+if(x=="2"){xxx="/2020/12/cara-membangun-bisnis-bagaimana-mulai.html"}a_to_ve[i].href="https://axsalrubiansyah.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
