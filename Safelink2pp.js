cc7a3dd80330cd1ae0cd5a123d987996d5941143
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2023-02-05 19:48:32 +0700

    Update Safelink2pp.js

--- Safelink2pp.js
+++ Safelink2pp.js
@@ -21,8 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2021/02/4-top-business-ideas-that-will-boom-in-2021.html"}
-if(x=="2"){xxx="/2021/02/best-video-editing-app-for-android-2021.html"}a_to_ve[i].href="https://axsalrubiansyah.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2021/02/best-video-editing-app-for-android-2021.html"}a_to_ve[i].href="https://axsalrubiansyah.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
