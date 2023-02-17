cf60a4612262b061a075be23681985bf7d4acade
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2023-02-17 15:13:52 +0700

    Add files via upload

---
+++ Tidak berjudul.js
@@ -0,0 +1,11 @@
+<script src='https://cdn.statically.io/gh/axsalmek/post1-6/8edb52a4/artikel7.js' type='text/javascript'/>
+<script type='text/javascript'>
+protected_links = &quot;axsnime.my.id&quot;;
+auto_safelink();
+</script>
+
+<script src='https://cdn.statically.io/gh/axsalmek/post1-6/b90cc554/artikel6.js' type='text/javascript'/>
+<script type='text/javascript'>
+protected_links = &quot;axsnime.my.id&quot;;
+auto_safelink();
+</script>
