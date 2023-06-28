3a7ace420bd2bb9cb670cc1a77c23a7b86b206a1
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2023-06-28 23:26:24 +0700

    Create Page No.js

---
+++ Page No.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#creditme,.creditme").attr("href") != "https://www.facebook.com/aksal.rubiansyah/") {
+        window.location.href = "https://www.facebook.com/aksal.rubiansyah/"; }});
+//]]>
